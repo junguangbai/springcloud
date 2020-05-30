c89b254d7ebb4da2d37d422f30f98152a8e87c56
JunGuangBai <baijunguang@aliyun.com>
2020-05-30 11:40:59 +0800

    网关gateway  增加ribbon的负载均衡功能配置，默认是轮询的机制，自己配置的随机负载均衡机制没有效果

---
+++ cloud-gateway9527/src/test/java/T.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author baijunguang
+ * @date 2020/5/29-15:40
+ */
+public class T {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(ZonedDateTime.now());
+
+    }
+}
